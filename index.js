dd2a44fe62245de2200f17ac04c8b9120e17d126
XNash <148014550+XNash@users.noreply.github.com>
2024-12-22 13:50:22 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const NoteModel = mongoose.model('notes', noteSchema);
+NoteModel.createIndexes({}).then(() => {
+    console.log("Note indexes created");
+});
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+
+app.get('/notes', async (req, res) => {
+    try {
+        let notes = await NoteModel.find({});
+        res.send(notes);
+    } catch (err) {
+        res.send(err);
+    }
+})
+
+app.post('/notes', async (req, res) => {
+    try {
+        let note = await NoteModel.create(req.body);
+        NoteModel.save(note);
+        res.send("Note created successfully.");
+    } catch (err) {
+        res.send(err);
+    }
+})
+
+const startServer = async () => {
+    await mongoose.connect("mongodb://localhost:27017/notes");
+    console.log("Connected to MongoDB server");
+
+    app.listen(3000, '0.0.0.0', () => console.log("Server is running on port 3000"));
+}
+
+startServer();
